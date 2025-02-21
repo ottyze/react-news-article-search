9129ff9c3d21baad4aff37b80ed05a98a14c1b42
Isaya <145881678+ottyze@users.noreply.github.com>
2025-02-21 00:26:57 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { IoIosBookmark } from "react-icons/io";
 import Search from "./Search";
 import Preferences from "./Preferences";
-import ArticleCard from "./components/ArticleCard";
-import FullArticle from "./components/FullArticle";
-import User, { UserContext } from "./components/User";
 
+import User from "./components/User";
 import ArticleList from "./components/ArticleList";
 
 /**
@@ -26,64 +24,41 @@ import ArticleList from "./components/ArticleList";
  * @prop {number} wordCount
  * @prop {number} sentenceCount
  */
-const SENTENCE_REGEX = /^\w+[.:;?!]/i;
-const SYLLABLE_REGEX = /((?!ed|es|e$)[aeiou]{1,2})+/i;
 
 function App() {
-    const [selectedArticle, setSelectedArticle] = useState(null);
     const [searchLevel, setSearchLevel] = useState("");
     const [showPreferences, setShowPreferences] = useState(false);
-    
 
-
-
-    const onCloseArticle = () => {
-        setSelectedArticle(null);
-    };
-    
     return (
         <div className="app">
             <User>
-                {!selectedArticle && (
-                    <div id="search-container">
-                        <div id="search-header">
-                            <header className="App-header">
-                                <div className="title-container">
-                                    <h1 className="pgTitle">
-                                        <IoIosBookmark
-                                            size={40}
-                                            color="#2b90ee"
-                                        />
-                                        <em className="bold">Article</em>Search
-                                    </h1>
-                                    <h2 className="authors">by Isaya Danice</h2>
-                                </div>
-                                    <Search
-                                        searchLevel={searchLevel}
-                                        setSearchLevel={setSearchLevel}
-                                    />
-                            </header>
-                            <Preferences
-                                showPreferences={showPreferences}
-                                onClick={() =>
-                                    setShowPreferences(!showPreferences)
-                                }
-                                onClose={() => setShowPreferences(false)}
+                <div id="search-container">
+                    <div id="search-header">
+                        <header className="App-header">
+                            <div className="title-container">
+                                <h1 className="pgTitle">
+                                    <IoIosBookmark size={40} color="#2b90ee" />
+                                    <em className="bold">Article</em>Search
+                                </h1>
+                                <h2 className="authors">by Isaya Danice</h2>
+                            </div>
+                            <Search
+                                searchLevel={searchLevel}
+                                setSearchLevel={setSearchLevel}
                             />
-                        </div>
-                        <div id="search-article-list">
-                            <ul>
-                                <ArticleList searchLevel={searchLevel} onSelectArticle={setSelectedArticle}/>
-                            </ul>
-                        </div>
+                        </header>
+                        <Preferences
+                            showPreferences={showPreferences}
+                            onClick={() => setShowPreferences(!showPreferences)}
+                            onClose={() => setShowPreferences(false)}
+                        />
                     </div>
-                )}
-                {selectedArticle && (
-                    <FullArticle
-                        article={selectedArticle}
-                        onClose={onCloseArticle}
-                    />
-                )}
+                    <div id="search-article-list">
+                        <ul>
+                            <ArticleList searchLevel={searchLevel} />
+                        </ul>
+                    </div>
+                </div>
             </User>
         </div>
     );
